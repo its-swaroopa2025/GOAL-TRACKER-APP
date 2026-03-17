541e696487af48476ca40f61cf7dea948c948fe2
Swarooparani Devineedi <swarooparanidevineedi@gmail.com>
2026-03-17 16:55:52 +0000

    Fix delete button functionality and update goal messages

--- script.js
+++ script.js
@@ -3,13 +3,13 @@ const errorLabel = document.querySelector('.error-label')
 const progressLabel = document.querySelector('.progress-label')
 const progressValue = document.querySelector('.progress-value')
 const addGoalBtn = document.querySelector(".btn")
-const deleteButton=document.querySelectorAll(".delete")
+
 
 const allQuotes = [
   'Raise the bar by completing your goals',
   'Well begun is half done',
   'Just a step away, keep going',
-  'Whoa! You just completed all the goals, time for chill dude'
+  'Whoa! You just completed all the goals, time for chill dude..'
 ]
 
 // YOU SHOULD Load stored goals..
@@ -71,7 +71,9 @@ appContainer.addEventListener('click', function(e){
 
   goalContainer.remove()
 
+  if(allGoals[id]){
   delete allGoals[id]
+}
 
   localStorage.setItem('DATA',JSON.stringify(allGoals))
   updateProgress()
@@ -185,7 +187,6 @@ Object.keys(allGoals).forEach((goalID)=>{
       type="text"
       value="${allGoals[goalID].name}"
       placeholder="Add a New Goal">
-       type="text" placeholder="Add a New Goal">
     <svg class="delete" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
   <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z"/>
   <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z"/>

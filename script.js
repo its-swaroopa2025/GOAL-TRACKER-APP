f43173f8314b6f1c74b42b2ba828903e3b85c019
its-swaroopa2025 <swarooparanidevineedi@gmail.com>
2026-03-17 20:29:01 +0000

    Renamed files and fixed delete functionality with localStorage sync

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const errorLabel = document.querySelector('.error-label')
 const progressLabel = document.querySelector('.progress-label')
 const progressValue = document.querySelector('.progress-value')
 const addGoalBtn = document.querySelector(".btn")
+const deleteButton=document.querySelectorAll(".delete")
 
 const allQuotes = [
   'Raise the bar by completing your goals',
@@ -60,6 +61,24 @@ appContainer.addEventListener('focusin', function(e){
 //------------------------------CHECKBOX LOGIC
 
 appContainer.addEventListener('click', function(e){
+
+  ///=-----------------DELETE LOGIC
+ const deleteBtn=e.target.closest('.delete')
+ if(deleteBtn){
+  const goalContainer=deleteBtn.parentElement
+  const input=goalContainer.querySelector('.goal-input')
+  const id=input.id
+
+  goalContainer.remove()
+
+  delete allGoals[id]
+
+  localStorage.setItem('DATA',JSON.stringify(allGoals))
+  updateProgress()
+  return
+
+ }
+  
 
   const checkbox = e.target.closest('.custom-checkbox')
   if(!checkbox) return
@@ -132,6 +151,10 @@ addGoalBtn.addEventListener("click", function(e){
     </div>
     <input id="${newGoalId}" class="goal-input"
     type="text" placeholder="Add a New Goal">
+    <svg class="delete" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+  <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z"/>
+  <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z"/>
+</svg>
   `
 
   const buttonContainer =
@@ -162,6 +185,11 @@ Object.keys(allGoals).forEach((goalID)=>{
       type="text"
       value="${allGoals[goalID].name}"
       placeholder="Add a New Goal">
+       type="text" placeholder="Add a New Goal">
+    <svg class="delete" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+  <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z"/>
+  <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z"/>
+</svg>
     `
 
     if(allGoals[goalID].completed){
@@ -179,6 +207,8 @@ Object.keys(allGoals).forEach((goalID)=>{
 })
 
 
+
+
 //----------------------------INITIAL PROGRESS  SECTION
 
 document.querySelectorAll('.goal-input').forEach((input) => {
@@ -194,4 +224,13 @@ document.querySelectorAll('.goal-input').forEach((input) => {
   }
 
 })
+<<<<<<< HEAD
 updateProgress()
+=======
+
+//--------------------DELETE-GOAL
+
+
+
+updateProgress()
+>>>>>>> a215637 (Renamed files and fixed delete functionality with localStorage sync)
